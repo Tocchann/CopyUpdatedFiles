5413f8c118899a4b6945bfa0136d47727022a875
Toshiyuki Takahagi <tosiyuki@hh.iij4u.or.jp>
2023-09-11 10:10:44 +0900

    OwnerWindowのサーチコードを分離

--- Morrin.Extensions.WPF/SelectFolderDialog.cs
+++ Morrin.Extensions.WPF/SelectFolderDialog.cs
@@ -11,134 +11,121 @@ using static Morrin.Extensions.WPF.Interops.IFileOpenDialog;
 using static Morrin.Extensions.WPF.Interops.IShellItem;
 using System.Collections;
 
-namespace Morrin.Extensions.WPF
+namespace Morrin.Extensions.WPF;
+
+public class SelectFolderDialog : ISelectFolderDialog
 {
-	public class SelectFolderDialog : ISelectFolderDialog
+	public static IServiceCollection ConfigureServices( IServiceCollection services )
 	{
-		public static IServiceCollection ConfigureServices( IServiceCollection services )
-		{
-			services.AddSingleton<ISelectFolderDialog, SelectFolderDialog>();
-			return services;
-		}
+		services.AddSingleton<ISelectFolderDialog, SelectFolderDialog>();
+		return services;
+	}
 
-		public string? InitialFolder { get; set; }
-		public string? SelectedPath {get; set; }
-		public string? Title { get; set; }
+	public string? InitialFolder { get; set; }
+	public string? SelectedPath {get; set; }
+	public string? Title { get; set; }
 
-		public void AddPlace( string folder, ISelectFolderDialog.FDAP fdap )
-		{
-			m_places.Add( (folder, fdap) );
-		}
+	public void AddPlace( string folder, ISelectFolderDialog.FDAP fdap )
+	{
+		m_places.Add( (folder, fdap) );
+	}
 
-		public bool? ShowDialog()
+	public bool? ShowDialog()
+	{
+		var ownerWindow = Utilities.GetOwnerWindow();
+		var hwndSrc = System.Windows.Interop.HwndSource.FromVisual( ownerWindow ) as System.Windows.Interop.HwndSource;
+		return ShowDialog( hwndSrc?.Handle??IntPtr.Zero );
+	}
+	internal bool? ShowDialog( IntPtr ownerWindow )
+	{
+		// オーナーウィンドウの正規化
+		ownerWindow = NativeMethods.GetSafeOwnerWindow( ownerWindow );
+
+		IFileOpenDialog? dlg = new FileOpenDialog() as IFileOpenDialog;  //	IUnknown::QueryInterfaceを使ってインターフェースを特定する
+		if( dlg != null )
 		{
-			var ownerWindow = Application.Current.MainWindow;
-			foreach( Window window in Application.Current.Windows )
+			try
 			{
-				if( window.IsActive )
+				//	フォルダ選択モードに切り替え
+				dlg.SetOptions( FOS.FORCEFILESYSTEM | FOS.PICKFOLDERS );
+				//	以前選択されていたフォルダを指定
+				bool setFolder = false;
+				var item = CreateItem( SelectedPath );
+				if( item is not null )
 				{
-					ownerWindow = window;
-					break;
+					dlg.SetFolder( item );
+					Marshal.ReleaseComObject( item );
+					setFolder = true;
 				}
-			}
-			return ShowDialog( ownerWindow );
-		}
-		internal bool? ShowDialog( Window ownerWindow )
-		{
-			var hwndSrc = System.Windows.Interop.HwndSource.FromVisual( ownerWindow ) as System.Windows.Interop.HwndSource;
-			return ShowDialog( hwndSrc?.Handle??IntPtr.Zero );
-		}
-		internal bool? ShowDialog( IntPtr ownerWindow )
-		{
-			// オーナーウィンドウの正規化
-			ownerWindow = NativeMethods.GetSafeOwnerWindow( ownerWindow );
-
-			IFileOpenDialog? dlg = new FileOpenDialog() as IFileOpenDialog;  //	IUnknown::QueryInterfaceを使ってインターフェースを特定する
-			if( dlg != null )
-			{
-				try
+				//	まだフォルダを設定していない場合は初期フォルダを設定する
+				if( !setFolder )
 				{
-					//	フォルダ選択モードに切り替え
-					dlg.SetOptions( FOS.FORCEFILESYSTEM | FOS.PICKFOLDERS );
-					//	以前選択されていたフォルダを指定
-					bool setFolder = false;
-					var item = CreateItem( SelectedPath );
+					item = CreateItem( InitialFolder );
 					if( item is not null )
 					{
 						dlg.SetFolder( item );
 						Marshal.ReleaseComObject( item );
-						setFolder = true;
 					}
-					//	まだフォルダを設定していない場合は初期フォルダを設定する
-					if( !setFolder )
-					{
-						item = CreateItem( InitialFolder );
-						if( item is not null )
-						{
-							dlg.SetFolder( item );
-							Marshal.ReleaseComObject( item );
-						}
-					}
-					//	タイトル
-					if( !string.IsNullOrWhiteSpace( Title ) )
-					{
-						dlg.SetTitle( Title );
-					}
-					//	ショートカット追加
-					foreach( var place in m_places )
-					{
-						item = CreateItem( place.folder );
-						if( item is not null )
-						{
-							dlg.AddPlace( item, (IFileOpenDialog.FDAP)place.fdap );
-							Marshal.ReleaseComObject( item );
-						}
-					}
-					//	ダイアログを表示
-					var hRes = dlg.Show( ownerWindow );
-					if( NativeMethods.SUCCEEDED( hRes ) )
-					{
-						item = dlg.GetResult();
-						SelectedPath = item.GetName( SIGDN.FILESYSPATH );
-						Marshal.ReleaseComObject( item );
-						return true;
-					}
-					// キャンセル以外のエラーが来た場合はなにかしら問題ありなので例外を投げる
-					else if( hRes != NativeMethods.HRESULT_FROM_WIN32(NativeMethods.Win32Error.Cancelled) )
-					{
-						// ここは、正直例外を投げたほうがいいと思うがどうなんだろう？
-						throw new COMException( "IFileOpenDialog.Show()のエラー", hRes );
-					}
-					return false;
 				}
-				finally
+				//	タイトル
+				if( !string.IsNullOrWhiteSpace( Title ) )
 				{
-					Marshal.FinalReleaseComObject( dlg );
+					dlg.SetTitle( Title );
 				}
+				//	ショートカット追加
+				foreach( var place in m_places )
+				{
+					item = CreateItem( place.folder );
+					if( item is not null )
+					{
+						dlg.AddPlace( item, (IFileOpenDialog.FDAP)place.fdap );
+						Marshal.ReleaseComObject( item );
+					}
+				}
+				//	ダイアログを表示
+				var hRes = dlg.Show( ownerWindow );
+				if( NativeMethods.SUCCEEDED( hRes ) )
+				{
+					item = dlg.GetResult();
+					SelectedPath = item.GetName( SIGDN.FILESYSPATH );
+					Marshal.ReleaseComObject( item );
+					return true;
+				}
+				// キャンセル以外のエラーが来た場合はなにかしら問題ありなので例外を投げる
+				else if( hRes != NativeMethods.HRESULT_FROM_WIN32(NativeMethods.Win32Error.Cancelled) )
+				{
+					// ここは、正直例外を投げたほうがいいと思うがどうなんだろう？
+					throw new COMException( "IFileOpenDialog.Show()のエラー", hRes );
+				}
+				return false;
 			}
-			return null;	//	ダイアログが用意できない場合(ここでは例外を投げない)
-		}
-		public SelectFolderDialog( ILogger<SelectFolderDialog> logger )
-		{
-			m_logger = logger;
-			m_places = new List<(string folder, ISelectFolderDialog.FDAP fdap)>();
-		}
-		/// <summary>
-		/// SHCreateItemFromParseName() のラッパー。
-		/// ファイルパスから、IShellItem を作成する専用メソッドとして用意。
-		/// </summary>
-		private static IShellItem? CreateItem( string? folder )
-		{
-			if( !string.IsNullOrWhiteSpace( folder ) &&
-				NativeMethods.SUCCEEDED(
-					NativeMethods.SHCreateItemFromParsingName( folder,
-						IntPtr.Zero, typeof( IShellItem ).GUID, out var item ) ) )
+			finally
 			{
-				return item;
+				Marshal.FinalReleaseComObject( dlg );
 			}
-			return null;
 		}
-		private List<(string folder, ISelectFolderDialog.FDAP fdap)> m_places;
-		private ILogger<SelectFolderDialog> m_logger;
+		return null;	//	ダイアログが用意できない場合(ここでは例外を投げない)
 	}
+	public SelectFolderDialog( ILogger<SelectFolderDialog> logger )
+	{
+		m_logger = logger;
+		m_places = new List<(string folder, ISelectFolderDialog.FDAP fdap)>();
+	}
+	/// <summary>
+	/// SHCreateItemFromParseName() のラッパー。
+	/// ファイルパスから、IShellItem を作成する専用メソッドとして用意。
+	/// </summary>
+	private static IShellItem? CreateItem( string? folder )
+	{
+		if( !string.IsNullOrWhiteSpace( folder ) &&
+			NativeMethods.SUCCEEDED(
+				NativeMethods.SHCreateItemFromParsingName( folder,
+					IntPtr.Zero, typeof( IShellItem ).GUID, out var item ) ) )
+		{
+			return item;
+		}
+		return null;
+	}
+	private List<(string folder, ISelectFolderDialog.FDAP fdap)> m_places;
+	private ILogger<SelectFolderDialog> m_logger;
 }
---
+++ Morrin.Extensions.WPF/Utilities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Morrin.Extensions.WPF;
+
+public static class Utilities
+{
+	public static Window GetOwnerWindow()
+	{
+		var ownerWindow = Application.Current.MainWindow;
+		foreach( Window window in Application.Current.Windows )
+		{
+			if( window.IsActive )
+			{
+				ownerWindow = window;
+				break;
+			}
+		}
+		return ownerWindow;
+	}
+}
